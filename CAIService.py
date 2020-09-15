abbd20a3d1ebb91111a0fc41180eecc9a1d8921b
Abhishek Dubey <abhi.dubey011999@gmail.com>
2020-09-15 13:04:34 +0530

    Moved command oprations startup logic to main console
    Signed-off-by: Abhishek Dubey <abhi.dubey011999@gmail.com>

--- CAIService.py
+++ CAIService.py
@@ -1,6 +1,5 @@
 import time
 import os
-import signal
 import subprocess as sp
 
 import win32.servicemanager as sm
@@ -27,24 +26,6 @@ class HandleLocalServer:
         self.isrunning = False
 
 
-class HandleCommandRecognizer:
-    def __init__(self):
-        self.dir_path = os.path.join(_CAI_DIR, "command_recognizer")
-        self.script_path = os.path.join(self.dir_path, "recognize_command.py")
-        self.isrunning = False
-
-    def run(self):
-        sm.LogInfoMsg("CAI: SERVICE: Starting command recognizer...")
-        self.proc = sp.Popen(["python", self.script_path], cwd=self.dir_path)
-        sm.LogInfoMsg(f"CAI: SERVICE: Recognizer started with pid {self.proc.pid}")
-        self.isrunning = True
-
-    def stop(self):
-        sm.LogInfoMsg("CAI: SERVICE: Stopping recognizer...")
-        self.proc.terminate()
-        self.isrunning = False
-
-
 class CAIService(PyWinService):
     _svc_name_ = "CAI Service"
     _svc_display_name_ = "CAI Service"
@@ -53,7 +34,6 @@ class CAIService(PyWinService):
     def start(self):
         self.isrunning = True
         self.server = HandleLocalServer()
-        self.recognizer = HandleCommandRecognizer()
 
     def stop(self):
         self.isrunning = False
@@ -62,10 +42,7 @@ class CAIService(PyWinService):
         while self.isrunning:
             if not self.server.isrunning:
                 self.server.run()
-            if not self.recognizer.isrunning:
-                self.recognizer.run()
             time.sleep(1)
-        self.recognizer.stop()
         self.server.stop()
 
 
